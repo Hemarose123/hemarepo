3dc07cde02ba3ac9e43210fd6079b6a765a83d26
Hemarose123 <163624741+Hemarose123@users.noreply.github.com>
2024-10-20 20:35:44 +0530

    Create details.py

---
+++ set 1/details.py
@@ -0,0 +1,40 @@
+##problem Statement
+Write a program to store the below details with separate variables(name,age,dob,height,weight,degree,gender,is_alive).
+set the value with appropriate types based on the value.say aghe should be an integer,etc.
+
+
+name=input("ENTER YOUR NAME:")
+age=int(input("ENTER YOUR AGE:"))
+dob=input("ENTER YOUR DATE OF BRITH:")
+height=input("ENTER YOUR HEIGHT:")
+weight=int(input("ENTER YOUR WEIGHT:"))
+degree=input("ENTER YOUR DEGREE:")
+gender=input("ENTER YOUR GENDER:")
+is_alive=True
+print("YOUR NAME IS",name)
+print("YOUR AGE IS",age)
+print("YOUR DATE OF BRITH IS",dob)
+print("YOUR HEIGHT IS",height)
+print("YOUR WEIGHT IS",weight)
+print("YOUR DEGREE IS",degree)
+print("YOUR GENDER IS",gender)
+print("IS ALIVE IS",is_alive)
+
+
+##console output
+
+ENTER YOUR NAME:HEMALATHA
+ENTER YOUR AGE:20
+ENTER YOUR DATE OF BRITH:20-06-2004
+ENTER YOUR HEIGHT:4.8
+ENTER YOUR WEIGHT:45
+ENTER YOUR DEGREE:BACHELOR'S
+ENTER YOUR GENDER:FEMALE
+YOUR NAME IS HEMALATHA
+YOUR AGE IS 20
+YOUR DATE OF BRITH IS 20-06-2004
+YOUR HEIGHT IS 4.8
+YOUR WEIGHT IS 45
+YOUR DEGREE IS BACHELOR'S
+YOUR GENDER IS FEMALE
+IS ALIVE IS True
