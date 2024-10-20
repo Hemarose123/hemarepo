d42dbc3ae8593b129d9979c585a9cee24701ed5a
Hemarose123 <163624741+Hemarose123@users.noreply.github.com>
2024-10-20 20:14:31 +0530

    Create pattern.py

---
+++ set 1/pattern.py
@@ -0,0 +1,15 @@
+##Problem Statement
+Write a program to print a simple pattern using asterisks or any other character.
+
+
+n=5
+for i in range(1,n):
+    print('*'*i)
+
+
+##console output
+
+*
+**
+***
+****
