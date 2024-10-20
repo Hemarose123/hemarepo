03e323c64a857cbae3f037e6b507a349e57f9dc0
Hemarose123 <163624741+Hemarose123@users.noreply.github.com>
2024-10-20 19:17:08 +0530

    Create name+age.py

---
+++ set 1/name+age.py
@@ -0,0 +1,14 @@
+##Problem Statement
+Write a program to print your name and age to the console on separate lines.
+
+
+name="HEMALATHA"
+age=20
+print(name)
+print(age)
+
+
+##console output
+
+HEMALATHA
+20
