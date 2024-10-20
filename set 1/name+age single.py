06dae31dafc7f4a68f0262472b19916bfa7b66d0
Hemarose123 <163624741+Hemarose123@users.noreply.github.com>
2024-10-20 19:50:04 +0530

    Create name+age single.py

---
+++ set 1/name+age single.py
@@ -0,0 +1,13 @@
+##Problem Statement
+Modify the program to print a message that combines your name and age in a single line using string concatenation.
+
+
+name="HEMALATHA"
+age=20
+message="MY NAME IS "   +name+    " AND I AM "   +str(age)+  " YEARS OLD."
+print(message)
+
+
+##console output
+
+MY NAME IS HEMALATHA AND I AM 20 YEARS OLD.
