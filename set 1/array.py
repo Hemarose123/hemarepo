22f4a59094a9a77420e99ea21c72eb14d2955ec8
Hemarose123 <163624741+Hemarose123@users.noreply.github.com>
2024-10-20 20:11:14 +0530

    Create array.py

---
+++ set 1/array.py
@@ -0,0 +1,14 @@
+##Problem Statement
+Declare an array of strings and print each element of the array to the console.
+
+
+flowers=["rose","jasmine","lily"]
+print("flowers:")
+for flower in flowers:
+    print(flower,end='\t')
+
+
+##Console Output
+
+flowers:
+rose  jasmine  lily
