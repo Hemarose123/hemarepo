e8a53661e526037f30032d4fa2b5bd438af16207
Hemarose123 <163624741+Hemarose123@users.noreply.github.com>
2024-10-20 20:23:34 +0530

    Create change var.py

---
+++ set 1/change var.py
@@ -0,0 +1,32 @@
+##Problem Statement
+Declare a variable name and assign it a numerical value.
+Declare a variable isStudent and assign it a boolean  value
+  print the value of each variable to the console.
+  change the value of the name variable to another string.
+Increment the value age variable by 1.
+Toggle the value  of the isStudent variable(from true to false or vice versa).
+
+
+
+name="HEMALATHA"
+age=20
+isStudent=True
+print("YOUR NAME IS",name)
+print("YOUR AGE IS",age)
+print("IS STUDENT:",isStudent)
+name="ROSE"
+age=age+1
+isStudent=not isStudent
+print("CHANGED NAME:",name)
+print("INCREMENTED AGE:",age)
+print("TOGGLE IS STUDENT:",isStudent)
+
+
+##console output
+
+YOUR NAME IS HEMALATHA
+YOUR AGE IS 20
+IS STUDENT:True
+CHANGED NAME:ROSE
+INCREMENTED AGE:21
+TOGGLE IS STUDENT:False
