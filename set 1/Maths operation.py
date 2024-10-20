db957908000a92b3f9e5c7e5fba2de009a30f6e3
Hemarose123 <163624741+Hemarose123@users.noreply.github.com>
2024-10-20 20:03:55 +0530

    Create Maths operation.py

---
+++ set 1/Maths operation.py
@@ -0,0 +1,27 @@
+##Problem Statement
+Write a program that calculates and print them to the console.
+
+
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
+print("Modulus:",mod)
+
+
+##console output
+
+enter the first number:30
+enter the second number:20
+Addition:50
+Subtraction:10
+Multiplication:600
+Division:1.5
+Modulud:10
