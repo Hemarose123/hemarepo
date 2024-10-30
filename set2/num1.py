ee2fcadcd0f49a43bf4502d94da381a1f3845c61
Hemarose123 <hemarose290@gmail.com>
2024-10-30 14:32:31 +0530

    python
    python code

---
+++ set2/num1.py
@@ -0,0 +1,13 @@
+##Declare different variables with meaningful names that follow naming conventions (camelCase, under_score_case).
+myname="HEMALATHA"
+myage=20
+myfavoritecolor="LIGHT PINK"
+your_name="ROSE"
+your_age=22
+your_favorite_color="BLUE"
+print("my name is",myname)
+print("my age is",myage)
+print("my favorite color is",myfavoritecolor)
+print("your name is",your_name)
+print("your age is",your_age)
+print("your favorite color is",your_favorite_color)
---
+++ set2/num9.py
@@ -0,0 +1,10 @@
+##Use the assignment operators (=, +=, -=, *=, /=) to modify the value of a variable and print the result.
+number=50
+number=number+10
+print("after +=10:",number)
+number=number-5
+print("after -=5:",number)
+number=number*2
+print("after *=2:",number)
+number=number/5
+print("after /=5:",number)
